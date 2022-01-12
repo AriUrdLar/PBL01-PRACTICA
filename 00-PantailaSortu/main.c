0f9c83c0d71443fe7ee495fe15bc379f1357c06a
Aritz <Ari_urd_lar@outlook.com>
2022-01-12 09:56:10 +0100

    MAPA

--- 00-PantailaSortu/main.c
+++ 00-PantailaSortu/main.c
@@ -1,43 +1,35 @@
-#include<stdio.h>
-#include<SDL.h>
-#include <stdlib.h>
-#include<SDL_image.h>
-#define WINDOW_WIDTH     500
-#define WINDOW_HEIGHT    500
-
+#include"Juego.h"
 
 int main(int argc, char* argv[])
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* lehioa = NULL;
-	SDL_Renderer* renderer = NULL;
-	int zikloa = 0;
-	SDL_Event event;//eventoen estruktura
+	char* link[20][20] = {
+							{"img/GRASS.png","img/WATER.png","img/MOUNTAIN.png","\0"},
+							{"img/PERTSONA3.png","img/PERTSONA2.png","img/PERTSONA.png","img/PERTSONA4.png"},
+							{"img/ORCO3.png","img/ORCO1.png","img/ORCO2.png","img/ORCO4.png"},
+							{"img/ISOTILE_NEO.png","img/ISOTILE1.png","img/RANGO.png","\0"}
+						};
+	mapPos.x = 500;
+	mapPos.y = 1000;
+	persona.x = 0;
+	persona.y = 0;
+	soldado.orientazioa = 0;
+	orco.orientazioa = 0;
+	init("tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1300, 800, false);
 
-
-	//LEHIO BAT SORTZEN DA, TITULO BATEKIN,TAMAINA BATEKIN ETA GERO FLAGS=ZE EZAUGARRIEKIN
-	lehioa = SDL_CreateWindow("hola Aritz", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
-
-	//POR LO QUE HE ENTENDIDO RENDER DA PARA MODIFICAR ALGO ORIGINAL, BERAZ HEMEN AURRETIK SORTUTAKO LEHIOARI ONDORENGO PROPIETATEAK APLIKATZEKO ESATEN DA	
-	renderer = SDL_CreateRenderer(lehioa, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE | SDL_RENDERER_PRESENTVSYNC);
-
-	//HIRU HAUEK BATEA DIJOZTE, PANTALLAN KOLOREA ALDATZEKO= EZDAKIT ZEBA	
-	SDL_SetRenderDrawColor(renderer, 0x3b, 0x3b, 0x3b, 0x00);
-	SDL_RenderClear(renderer);
-	SDL_RenderPresent(renderer);
-
-	//PANTALLA IXTEKO,MINIMIZATZEKO EDO MUGITZEKO
-	while (!zikloa)
+	irudiakKargatu(terreno.irudiak, link[0]);
+	irudiakKargatu(soldado.irudiak, link[1]);
+	
+	irudiakKargatu(orco.irudiak, link[2]);
+	irudiakKargatu(cubo.irudiak, link[3]);
+	while (isRunning)
 	{
-		//PANTALLA BARRUAN EBENTORENBAT GERTATZEN DEN BITARTEAN
-		while (SDL_PollEvent(&event) != 0)//EVENT INIZIATZEKO DERRIGORREZKOA OMEN DA HAU ITEA
-		{
-			if (event.type == SDL_QUIT)zikloa = 1;
-			else if (event.key.keysym.sym == SDLK_ESCAPE)zikloa = 1;// hau dao estruktura baten anidatuaren, anidatuaren..anidatuan.
-		}
+		handleEvents();
+		update();
+		render();
 	}
-	SDL_DestroyWindow(lehioa);
-	SDL_Quit();
-
+	clean();
 	return 0;
+}
+void irudiakKargatu(SDL_Texture*gotorlekua[] ,char** link) {
+	for (int i = 0; i < 4; i++)gotorlekua[i] = loadImage(link[i], renderer);
 }
